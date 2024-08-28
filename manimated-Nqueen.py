7a3227285d67641e481538aab6a0bf89c5e37ed0
ssentongo josh <ssentongoj05hu4@gmail.com>
2024-08-28 17:17:41 +0300

    made all the threatened sets global to the solution function

--- manimated-Nqueen.py
+++ manimated-Nqueen.py
@@ -3,6 +3,9 @@ from chess_board import ChessBoard
            
 class NqueenVisualization(Scene):
     boardStatus = {}
+    threatened_columns_set = set()
+    threatened_negative_diagonals_set = set()
+    threatened_positive_diagonals_set = set()
 
     def construct(self):
         N = 3
@@ -12,30 +15,28 @@ class NqueenVisualization(Scene):
         self.solution(N,1)
 
     def solution(self,N,row):
-        threatened_columns_set = set()
-        threatened_negative_diagonals_set = set()
-        threatened_positive_diagonals_set = set()
+        
         result = []
 
         if row == N+1:
             return 
         
         for column in range(N):
-            if column in threatened_columns_set or (row - column) in threatened_negative_diagonals_set or (row + column) in threatened_positive_diagonals_set:
+            if column in NqueenVisualization.threatened_columns_set or (row - column) in NqueenVisualization.threatened_negative_diagonals_set or (row + column) in NqueenVisualization.threatened_positive_diagonals_set:
                 continue
             #place queen and update sets
             self.main(row,column)
-            threatened_columns_set.add(column)
-            threatened_negative_diagonals_set.add(row - column)
-            threatened_positive_diagonals_set.add(row + column)
+            NqueenVisualization.threatened_columns_set.add(column)
+            NqueenVisualization.threatened_negative_diagonals_set.add(row - column)
+            NqueenVisualization.threatened_positive_diagonals_set.add(row + column)
 
             #recursive call function to place next queen
             self.solution(N,row + 1)
 
             # backtrack: remove queen and update sets
-            threatened_columns_set.remove(column)
-            threatened_negative_diagonals_set.remove(row - column)
-            threatened_positive_diagonals_set.remove(row + column)
+            NqueenVisualization.threatened_columns_set.remove(column)
+            NqueenVisualization.threatened_negative_diagonals_set.remove(row - column)
+            NqueenVisualization.threatened_positive_diagonals_set.remove(row + column)
             
             # create function that removes a placed queen
             self.main(row,column,remove=True)
